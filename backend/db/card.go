73294f5a3de3d799716476f6bc40216ea6d690d6
Rohit Awate <rohitawate121@gmail.com>
2023-02-17 21:08:31 -0500

    Load foreign keys (owner) along with card

--- backend/db/card.go
+++ backend/db/card.go
@@ -1,12 +1,15 @@
 package db
 
-import "CardHero/models"
+import (
+	"CardHero/models"
+	"gorm.io/gorm/clause"
+)
 
 func GetCardsBy(user models.User) ([]models.Card, error) {
 	conn := getConn()
 
 	var cards []models.Card
-	err := conn.Find(&cards, "owner_id = ?", user.ID).Error
+	err := conn.Preload(clause.Associations).Find(&cards, "owner_id = ?", user.ID).Error
 	if err != nil {
 		return nil, err
 	}
