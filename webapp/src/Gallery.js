2bec82aed35035d05829306fdc0118dec12cf2cb
Rohit Awate <rohitawate121@gmail.com>
2023-02-17 22:48:54 -0500

    Add loading ellipsis, change and add shadows to cards and chat bubbles

--- webapp/src/Gallery.js
+++ webapp/src/Gallery.js
@@ -6,7 +6,16 @@ import Card from "./Card";
 class Gallery extends Component {
     render() {
         return <div id="gallery">
-            {this.props.cards.map(card => <Card card={card}/>)}
+            {
+                this.props.cards.length > 0 ?
+                    this.props.cards.map(card => <Card key={card.id} card={card}/>)
+                    : <img
+                        className="loader"
+                        src="/loaders/ellipsis-green.svg"
+                        alt="Loading..."
+                        style={{alignSelf: "center"}}
+                    />
+            }
         </div>;
     }
 }
