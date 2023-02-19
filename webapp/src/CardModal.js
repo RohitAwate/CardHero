97cf60a827001c3d38c3ffaf241fc7f37dc45c6a
Rohit Awate <rohitawate121@gmail.com>
2023-02-18 22:00:48 -0500

    CardModal: Implement folder path links

--- webapp/src/CardModal.js
+++ webapp/src/CardModal.js
@@ -4,10 +4,26 @@ import "./CardModal.css";
 
 class CardModal extends Component {
     render() {
+        const folders = ["Folder", "Sub-folder", "Another sub-folder"];
+
         return <div className="card-modal-container" onClick={this.props.onExit}>
             <div className="card-modal" onClick={event => event.stopPropagation()}>
                 <div className="card-modal-nav-bar">
-                    <p>Folder > SubFolder > Another SubFolder</p>
+                    <div>
+                        {
+                            folders.map((folder, i, row) => {
+                                const link = <a className="folder-link" href="#">{folder}</a>;
+                                if (i + 1 === row.length) {
+                                    // Last one.
+                                    return link;
+                                } else {
+                                    // Not last one.
+                                    return [link, "  >  "];
+                                }
+                            })
+                        }
+                    </div>
+
                     <button onClick={this.props.onExit} className="transparent-btn">
                         <img src="/icons/close-cross-48.png" alt="close-cross-icon"/>
                     </button>
