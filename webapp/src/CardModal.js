1dc6aa0e8f156dafe8b48e970da97f2f4ef13439
Rohit Awate <rohitawate121@gmail.com>
2023-02-19 00:04:48 -0500

    CardModal: Move the folders list to bottom, overlay the cross to close modal and make it appear on hover

--- webapp/src/CardModal.js
+++ webapp/src/CardModal.js
@@ -1,15 +1,42 @@
 import React, {Component} from "react";
 
 import "./CardModal.css";
+import Card from "./Card";
 
 class CardModal extends Component {
+    state = {
+        mouseInside: false
+    }
+
+    onMouseMovement = (inside) => {
+        console.log("inside:", inside)
+        this.setState({mouseInside: inside});
+    }
+
     render() {
+        const card = this.props.card;
         const folders = ["Folder", "Sub-folder", "Another sub-folder"];
 
         return <div className="card-modal-container" onClick={this.props.onExit}>
-            <div className="card-modal" onClick={event => event.stopPropagation()}>
+            <div
+                className="card-modal"
+                onClick={event => event.stopPropagation()}
+                onMouseOver={_ => this.onMouseMovement(true)}
+                onMouseLeave={_ => this.onMouseMovement(false)}
+            >
                 <div className="card-modal-nav-bar">
-                    <div>
+                    {
+                        this.state.mouseInside ?
+                            <button onClick={this.props.onExit} className="transparent-btn">
+                                <img src="/icons/close-cross-48.png" alt="close-cross-icon"/>
+                            </button> : ""
+                    }
+                </div>
+                <div className="card-modal-body">
+                    <p>{card.contents}</p>
+                </div>
+                <div className="card-modal-bottom-bar">
+                    <div className="card-modal-nav-bar-folders-container">
                         {
                             folders.map((folder, i, row) => {
                                 const link = <a className="folder-link" href="#">{folder}</a>;
@@ -24,12 +51,7 @@ class CardModal extends Component {
                         }
                     </div>
 
-                    <button onClick={this.props.onExit} className="transparent-btn">
-                        <img src="/icons/close-cross-48.png" alt="close-cross-icon"/>
-                    </button>
-                </div>
-                <div className="card-modal-body">
-                    <p>{this.props.card.contents}</p>
+                    <p className="timestamp">{Card.renderTimestamp(card.timestamp)}</p>
                 </div>
             </div>
         </div>;
