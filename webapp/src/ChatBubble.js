67ab211523e20bdee6b25e080b43dd30372f91fc
Rohit Awate <rohitawate121@gmail.com>
2023-07-01 14:50:39 -0700

    Implement opening a card by clicking on its chat bubble

--- webapp/src/ChatBubble.js
+++ webapp/src/ChatBubble.js
@@ -1,12 +1,37 @@
-import {Component} from "react";
+import React, {Component} from "react";
 
 import "./ChatBubble.css";
 import "./Common.css";
+import axios from "axios";
+import {useNavigate} from "react-router-dom";
 
-class ChatBubble extends Component {
+function ChatBubble(props) {
+    const navigate = useNavigate();
+    const openCardHandler = async (e) => {
+        const resp = await axios.get(`/api/rohit/card/goto/${props.card.id}`);
+        if (resp.status === 200) {
+            return navigate(resp.data);
+        }
+    };
+
+    return <div className="chat-bubble-container">
+        <ChatBubbleNavigation openCardHandler={openCardHandler}/>
+        <ChatBubbleMeta card={props.card}/>
+    </div>;
+}
+
+function ChatBubbleNavigation(props) {
+    return <div onClick={props.openCardHandler} className="chat-bubble-nav">
+        <button className="transparent-btn">
+            <img src="/icons/external-link-60.png" alt="close-cross-icon"/>
+        </button>
+    </div>;
+}
+
+class ChatBubbleMeta extends Component {
     render() {
         const card = this.props.card;
-        const formattedTime = ChatBubble.renderTimestamp(card.timestamp);
+        const formattedTime = ChatBubbleMeta.renderTimestamp(card.timestamp);
 
         return <div key={card.id} className="chat-bubble">
             <p className="chat-bubble-contents">{card.contents}</p>
